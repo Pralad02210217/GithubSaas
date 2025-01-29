70486c20aaeb586567a951eb315b750c3b98392a
pralad02210217 <praladmishra3@gmail.com>
2025-01-29 19:06:49 +0600

    VectorEmbedding Implemented

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useUser } from "@clerk/nextjs"
 import { ExternalLink, Github } from "lucide-react"
 import Link from "next/link"
 import CommitLog from "./commit-log"
+import AskQuestionCard from "./ask-question-card"
 
 const DashboardPage = () => {
   const { project } = useProject()
@@ -37,7 +38,7 @@ const DashboardPage = () => {
       
       <div className="mt-4">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
-          AskingQuesitonCard
+          <AskQuestionCard />
           MeetingCard
         </div>
       </div>
