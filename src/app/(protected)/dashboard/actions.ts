ac776ba9e9c142bef1439dc4433a30a2b466c7f8
pralad02210217 <praladmishra3@gmail.com>
2025-01-30 19:26:01 +0600

    done with creating QuestionModel

---
+++ src/app/(protected)/dashboard/actions.ts
@@ -0,0 +1,79 @@
+'use server'
+import { Output, streamText } from 'ai'
+import { createStreamableValue } from 'ai/rsc'
+import { createGoogleGenerativeAI } from '@ai-sdk/google'
+import { generateEmbedding } from '@/lib/gemini'
+import { db } from '@/server/db'
+
+const google = createGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY
+})
+
+export async function askQuestion(question:string, projecId: string) {
+    const stream = createStreamableValue()
+    const queryVector = await generateEmbedding(question)
+    const vectorQuery = `[${queryVector.join(',')}]`
+
+    const result = await db.$queryRaw`
+    SELECT "fileName", "sourceCode", "summary",
+    1 - ("summaryEmbedding" <=> ${vectorQuery}::vector) AS similarity
+    FROM "SourceCodeEmbedding"
+    WHERE 1 - ("summaryEmbedding" <=> ${vectorQuery}:: vector) > 0.5
+    AND "projectId" = ${projecId}
+    ORDER BY similarity DESC
+    LIMIT 10
+    ` as {fileName: string; sourceCode: string; summary: string; similarity:number}[]
+    
+    let context =''
+    for (const doc of result){
+        context+= `source: ${doc.fileName}\ncode content: ${doc.sourceCode}\n summary of file: ${doc.summary}\n\n`
+    } 
+
+        (async () => {
+          const { textStream } = await streamText({
+            model: google('gemini-1.5-flash'),
+            prompt: `
+        You are an AI code assistant who answers questions about the codebase. Your target audience is a technical intern with limited prior knowledge.
+        
+        AI assistant is a brand new, powerful, human-like artificial intelligence.
+        
+        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+        
+        AI is a well-behaved and well-mannered individual.
+        
+        AI is always friendly, kind, and inspiring, and is eager to provide vivid and thoughtful responses to the user.
+        
+        AI should focus on providing clear, concise, and actionable guidance. When answering questions about code, AI should give examples or step-by-step explanations if relevant.
+        
+        If the question is asking about code or a specific file, AI will provide the detailed answer, giving step-by-step instructions.
+        
+        START CONTEXT BLOCK
+        ${context}
+        END OF CONTEXT BLOCK
+        
+        START QUESTION
+        ${question}
+        END OF QUESTION
+        
+        AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+        If the context does not provide the answer to the question, the AI assistant will say, "I'm sorry, but I don't know the answer."
+        AI assistant will not apologize for previous responses but instead will indicate when new information was gained.
+        AI assistant will not invent anything that is not drawn directly from the context.
+        
+        Answer in markdown syntax, with code snippets if needed. Be as detailed as possible when answering, ensuring clarity for the technical intern.
+            `
+          });
+          
+          for await (const delta of textStream){
+            stream.update(delta)
+          }
+          stream.done()
+        })();
+
+    return {
+        Output: stream,
+        fileReferenced: result
+    }
+    
+        
+}
--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,12 +1,49 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog'
+import { Textarea } from '@/components/ui/textarea'
 import useProject from '@/hooks/use-project'
-import React from 'react'
+import { DialogTitle } from '@radix-ui/react-dialog'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const AskQuestionCard = () => {
     const { project } = useProject()
+    const [open, setOpen] = useState(false)
+    const [question, setQuestion] = useState('')
+
+    const onSubmit = async(e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+        setOpen(true)
+    }
   return (
-    <div>AskQuestionCard</div>
+    <>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+            <DialogHeader>
+                <DialogTitle>
+                    <Image src='/logo.png' alt='githubsaas logo' width={32} height={32} />
+                </DialogTitle>
+            </DialogHeader>
+            </DialogContent>
+        </Dialog>
+        <Card className='relative col-span-3'>
+            <CardHeader>
+                <CardTitle>Ask a question</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form onSubmit={onSubmit}>
+                    <Textarea placeholder='Which file should i edit to change the home page?' value={question} onChange={e => setQuestion(e.target.value)} />
+                    <div className="h-4"></div>
+                    <Button type='submit'>
+                        Ask GithubAgent
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    </>
   )
 }
 
