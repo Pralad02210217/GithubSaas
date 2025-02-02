4830912c7e9d9fd885803187619813e73bf09533
pralad02210217 <praladmishra3@gmail.com>
2025-02-02 22:45:39 +0600

    Done with invite button

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -8,6 +8,7 @@ import CommitLog from "./commit-log"
 import AskQuestionCard from "./ask-question-card"
 import MeetingCard from "./meeting-card"
 import ArchiveButton from "./archive-button"
+import InviteButton from "./invite-button"
 
 const DashboardPage = () => {
   const { project } = useProject()
@@ -32,7 +33,7 @@ const DashboardPage = () => {
         <div className="h-4"></div>
         <div className="flex items-center gap-4">
           TeamMembers
-          InviteMembers
+          <InviteButton />
           <ArchiveButton />
         </div>
       </div>
