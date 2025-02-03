95772f601afd7874bbb0b57c6807b9c908c788fa
pralad02210217 <praladmishra3@gmail.com>
2025-02-03 22:56:00 +0600

    addead a button to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import DashboardPage from "./(protected)/dashboard/page";
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
 
   return (
-    <DashboardPage />
+    <>
+      <h1 className="text-red-600">Hello</h1>
+    <button onClick={() => {
+      redirect('/dashboard')
+    }}>
+      Click to go to dashboard
+    </button>
+    </>
   );
 }
